04f01cab5e4e0a2cbbe652fbd8c8b4e8e1987b77
riobotic <mario.reynoso.ii@gmail.com>
2021-12-06 16:41:56 -0500

    make it better

---
+++ server.js
@@ -0,0 +1,24 @@
+// REQUIRE DEPENDENCIES
+const express = require('express');
+const mongoose = require("mongoose");
+
+
+// INTIALIZE EXPRESS APP
+const app = express();
+
+// CONFIGURE SETTINGS
+
+// DATABASE CONFIGURATION
+
+const DATABASE_URL =
+  "mmongodb+srv://admin:abcd123@cluster0.emozf.mongodb.net/shoecollection?retryWrites=true&w=majority"
+const db = mongoose.connection
+mongoose.connect(DATABASE_URL)
+
+const PORT = 3000;
+
+
+
+app.listen(PORT, () => {
+    console.log('BADADADAAAA');
+})
