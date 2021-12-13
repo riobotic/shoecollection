220cfb214f758be8ea33bcb54a44382f418c7ba6
riobotic <mario.reynoso.ii@gmail.com>
2021-12-13 09:15:46 -0500

    WIP CSS

--- controllers/shoes.js
+++ controllers/shoes.js
@@ -12,8 +12,8 @@ shoesRouter.get('/', (req, res) => {
     Shoes.find({}, (err, allShoes) => {
       res.render("index.ejs", {
         shoe: allShoes
-      });
     });
+  });
 });
   // New
   shoesRouter.get("/new", (req, res) => {
@@ -29,6 +29,12 @@ shoesRouter.get('/', (req, res) => {
 
   // Update
   shoesRouter.put('/:id', (req, res) => {
+    if (req.body.completed === "on") {
+      req.body.completed = true
+    } else {
+      req.body.completed = false
+    }
+  
     Shoes.findByIdAndUpdate(
       req.params.id,
       req.body,
@@ -37,9 +43,8 @@ shoesRouter.get('/', (req, res) => {
       },
       (error, updatedShoes) => {
         res.redirect(`/shoecollection/${req.params.id}`)
-      }
-    )
-  });
+      });
+    });
 
   // Create
   shoesRouter.post('/', (req, res) => {
@@ -51,22 +56,24 @@ shoesRouter.get('/', (req, res) => {
   
     Shoes.create(req.body, (error, createdShoes) => {
       res.redirect("/shoecollection")
-    })
-  })
-  
+  });
+});
 
 // Edit
   shoesRouter.get('/:id/edit', (req, res) => {
     Shoes.findById(req.params.id, (err, editShoes) => {
-        res.render('edit.ejs', { editShoes });
-    });
+        res.render('edit.ejs', { 
+          shoe: editShoes });
+  });
 });
+
 // Show
   shoesRouter.get('/:id', (req, res) => {
     Shoes.findById(req.params.id, (err, foundShoes) => {
     res.render("show.ejs", {
       shoe: foundShoes
     });
-});
+  });
 }); 
+
   module.exports = shoesRouter;
